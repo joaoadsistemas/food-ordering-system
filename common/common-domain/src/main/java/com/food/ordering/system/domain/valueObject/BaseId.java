39cf8494c3d6809d17bd64fb6e9a76b2284e70ac
joaoadsistemas <joaoadsistemas@gmail.com>
2026-07-27 20:56:51 -0300

    feature(common-domain): creation of common event and value objects

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueObject/BaseId.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.domain.valueObject;
+
+import java.util.Objects;
+
+public abstract class BaseId<T> {
+    private final T value;
+
+    protected BaseId(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof BaseId<?> baseId)) return false;
+        return Objects.equals(value, baseId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
