c327dc57d5c09065ccfd955cb5b00d5038e5f8d7
joaoadsistemas <joaoadsistemas@gmail.com>
2026-09-03 17:53:09 -0300

    feature(restaurant-service): implementing restaurant-service container, messaging, domain and data access

---
+++ restaurant-service/restaurant-data-access/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/entity/OrderApprovalEntity.java
@@ -0,0 +1,25 @@
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.entity;
+
+
+import com.food.ordering.system.domain.valueObject.OrderApprovalStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_approval", schema = "restaurant")
+@Entity
+public class OrderApprovalEntity {
+
+    @Id
+    private UUID id;
+    private UUID restaurantId;
+    private UUID orderId;
+    @Enumerated(EnumType.STRING)
+    private OrderApprovalStatus status;
+}
